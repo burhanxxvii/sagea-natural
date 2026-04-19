6157d9c212940bde4c537d42452047a9c8098d1f
burhanxxvii <149396027+burhanxxvii@users.noreply.github.com>
2026-04-19 23:40:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,27 +4,3 @@ function orderProduct(productName) {
       "_blank"
     );
   }
-  
-  // FIX SCROLL ANIMATION
-  const elements = document.querySelectorAll('.fade-slide');
-  
-  function revealOnScroll() {
-    elements.forEach(el => {
-      const rect = el.getBoundingClientRect();
-      const windowHeight = window.innerHeight;
-  
-      if (rect.top < windowHeight - 50) {
-        const delay = el.dataset.delay || 0;
-  
-        setTimeout(() => {
-          el.classList.add('show');
-        }, delay);
-      }
-    });
-  }
-  
-  // jalan saat pertama load
-  window.addEventListener('load', revealOnScroll);
-  
-  // jalan saat scroll
-  window.addEventListener('scroll', revealOnScroll);
